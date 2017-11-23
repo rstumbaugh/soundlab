c5b1f87aeb5e2784d09e5dffbe4819f82172ecba
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-11-23 12:15:04 -0500

    Added test api call to lambda function

--- ebs/src/app.jsx
+++ ebs/src/app.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://sul0jd1ro0.execute-api.us-east-1.amazonaws.com/prod/testing')
+      .then((response) => {
+        this.setState({
+          message: response.data.message
+        });
+      }).catch((err) => {
+        this.setState({
+          message: err
+        });
+      });
   }
 
   render() {
-    return <h1>Hello, world!</h1>;
+    return <h1>{`Message: ${this.state.message}`}</h1>;
   }
 }
 
