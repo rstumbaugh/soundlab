4982cb07d69f049e7261700649292dbc3c85b79c
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2017-12-12 23:53:56 -0500

    add fontawesome to HTML, background styles to app.jsx

--- eb/src/app.jsx
+++ eb/src/app.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from 'pages/home.jsx';
 import Session from 'pages/session.jsx';
+import styleVars from 'styles/variables';
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Route exact path='/' component={Home} />
       <Route path='/session/:id' component={Session} />
+
+      <style type='text/css'>
+        { `html, body { height: 100%; background-color: ${styleVars.lightGray}` }
+      </style>
     </div>
   </BrowserRouter>
 );
